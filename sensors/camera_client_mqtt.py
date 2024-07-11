75a96d83bbb763b1fca0a77f24b594506886c434
CopelandMIT <dcope@mit.edu>
2024-07-11 10:56:29 -0400

    working on heartbeat

--- sensors/camera_client_mqtt.py
+++ sensors/camera_client_mqtt.py
@@ -9,14 +9,17 @@ class CameraClientMQTT:
     def __init__(self, client_id, broker_address = "192.168.68.125", port=1883) -> None:
         self.port = port
         self.broker_address = broker_address
-        self.client_id = client_id
         self.deployed_sensor_id = client_id
         self.client = None
+        self.is_recording = False
         self.stop_event = threading.Event()
+        self.status = "Not Connected"
 
+
+# SENSOR MQTT SET UP
 
     def handle_mqtt_operations(self):
-        self.client = mqtt.Client(client_id=self.client_id)  
+        self.client = mqtt.Client(client_id=self.deployed_sensor_id)  
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
@@ -25,15 +28,16 @@ class CameraClientMQTT:
             self.client.loop_start()       
             while not self.stop_event.is_set():
                 time.sleep(1)
+        except Exception as e:
+            print(f"Failed to connect to Broker: {e}")
         finally:
             self.client.loop_stop()
             self.client.disconnect()
             print("Disconnected from broker.")        
 
-
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            print(f"{self.client_id} is connected to broker!")
+            print(f"{self.deployed_sensor_id} is connected to broker!")
             self.subscribe_to_topics(["to_sensor/global/commands/start_recording",
                                       "to_sensor/global/commands/stop_recording",
                                       f"to_sensor/{self.deployed_sensor_id}/commands/confirm_connection",
@@ -43,75 +47,135 @@ class CameraClientMQTT:
         else:
             print(f"{self.client} unable to connect to broker with return code: {rc}")
 
-    def on_message(self, client, userdata, msg):
-        # confirm_connection logic
-        if "confirm_connection" in msg.topic:
-            print(f"Central Server to {self.deployed_sensor_id} connection confrimed!")
-            self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/confirm_connection", str({self.deployed_sensor_id}), qos=2)
-        # start_recording logic
-        elif "start_recording" in msg.topic:
-            print(f"Recieved message on {msg.topic}: {msg.payload.decode()}")
-            self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/update_from_sensor", json.dumps({"deployed_sensor_id":self.deployed_sensor_id, "message":"Recording Started"}), qos=2)
-            recording_instructions_from_central_server = json.loads(msg.payload)
-            self.start_recording(recording_instructions_from_central_server)
-        # status_update_request logic
-        # stop_recording logic
-
-    
     def subscribe_to_topics(self, topics):
         if isinstance(topics, str):
             topics = [topics]
         for topic in topics:
             self.client.subscribe(topic=topic)
-            print(f"{self.client_id} is subscribed to {topic}")
+            print(f"{self.deployed_sensor_id} is subscribed to {topic}")
+    
+    def on_message(self, client, userdata, msg):
+        topic = msg.topic
+        print(topic)
+        data = json.loads(msg.payload)
+        if topic.endswith("confirm_connection"):
+            print(f"Received payload: {msg.payload.decode()}")  # Decode bytes to string for readability
+            self.confirm_connection()
+        elif topic.endswith("start_recording"):
+            self.start_recording(data)
+        elif topic.endswith("stop_recording"):
+            self.stop_recording()
+        elif topic.endswith("status_update_request"):
+            self.send_status_update()
 
-    def start_recording(self, message_data):
-        delayed_start_timestamp = message_data['delayed_start_timestamp']
-        sensor_deployment_id = self.deployed_sensor_id
-        duration = message_data['duration']
-        filename = message_data['filename']
-        additional_info = message_data['additional_info']
+# CONFIRM CONNECTION WITH CENTRAL SERVER
 
-        cap = cv2.VideoCapture(0)
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        data_directory = os.path.join(os.getcwd(), 'data')
-        filepath = os.path.join(data_directory, f"{filename}.avi")
-        out = cv2.VideoWriter(filepath, fourcc, 20.0, (640, 480))
+    def confirm_connection(self):
+        print(f"Central Server to {self.deployed_sensor_id} connection confrimed!")
+        self.client.publish(
+            f"to_central_server/{self.deployed_sensor_id}/status/confirm_connection", 
+            str({self.deployed_sensor_id}), 
+            qos=2)
+        self.status = "Ready"
 
-        # Wait until the specified timestamp to start data collection
-        while time.time() < delayed_start_timestamp:
-            time.sleep(0.01)
+# SEND HEARTBEAT STATUS UPDATE
 
-        start_time = time.time()
-        print(f"Camera {sensor_deployment_id} Recording Started")
-        while time.time() - start_time < duration:
-            ret, frame = cap.read()
-            if ret:
-                out.write(frame)
-            else:
-                break
+        
+    def heartbeat_thread(self):
+        while not self.stop_event.is_set():
+            # Send heartbeat message
+            self.client.publish(f"sensor/{self.deployed_sensor_id}/heartbeat",
+                                 json.dumps({self.status}), 
+                                 qos=1)
+            print(f"Heartbeat sent from {self.deployed_sensor_id}")
+            time.sleep(10)  # Send heartbeat every 10 seconds
 
-        cap.release()
-        out.release()
-        print(f"Data collection completed at {time.time()}")
-        print(f"File saved: {filepath}")
-        filesize = self.get_file_size(filepath)
-        self.send_finished_recording_to_central_server(filename, filesize=filesize)
+# SEND REQUESTED STATUS UPDATE
+
+    def send_status_update(self):
+        # Send a regular status update about the recording status
+        self.status = "Recording" if self.is_recording else "Ready"
+        self.client.publish(
+            f"to_central_server/{self.deployed_sensor_id}/status/update_from_sensor",
+            json.dumps({"status": self.status}), 
+            qos=2)
         
 
-    def get_file_size(self, file_path):
-        '''Return the file size'''
+# CAMERA RECORDING
+
+    def start_recording(self, message_data):
+        threading.Thread(target=self.handle_recording, args=(message_data,)).start()
+
+    def confirm_start_recording(self):
+        self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/started_recording",\
+                                 json.dumps({
+                                    "deployed_sensor_id":self.deployed_sensor_id, 
+                                    "message":"Recording Started"}),
+                                qos=2)
+
+    def handle_recording(self, message_data):
+        self.is_recording = True
+        filepath = self._setup_camera(message_data['filename'])
+        self._perform_recording(message_data['duration'], 
+                                message_data['delayed_start_timestamp'], 
+                                filepath)
+        self._cleanup_camera(filepath)
+
+    def _setup_camera(self, filename):
+        self.cap = cv2.VideoCapture(0)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        data_directory = os.path.join(os.getcwd(), 'data')
+        if not os.path.exists(data_directory):
+            os.makedirs(data_directory)
+        filepath = os.path.join(data_directory, f"{filename}.avi")
+        self.out = cv2.VideoWriter(filepath, fourcc, 20.0, (640, 480))
+        return filepath
+
+    def _perform_recording(self, duration, delayed_start_timestamp, filepath):
+        print("Waiting for delayed start time")
+        while time.time() < delayed_start_timestamp:
+            time.sleep(0.01)
+        print("Recording started")
+        while time.time() - delayed_start_timestamp < duration:
+            ret, frame = self.cap.read()
+            if ret:
+                self.out.write(frame)
+            else:
+                break
+        print(f"Data collection completed at {time.time()}")
+        print(f"File saved: {filepath}")
+        self.is_recording = False
+
+    def _cleanup_camera(self, filepath):
+        self.cap.release()
+        self.out.release()
+        filesize = self._get_file_size(filepath)
+        self._send_finished_recording_message_to_central_server(filepath, filesize)
+
+    def _send_finished_recording_message_to_central_server(self, filename, filesize):
+        self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/finished_recording",
+                            json.dumps({"deployed_sensor_id": self.deployed_sensor_id,
+                                    "filename": filename,
+                                    "file_size": filesize}), 
+                            qos=2)
+
+    def _get_file_size(self, file_path):
         try:
-            size = os.path.getsize(file_path)
-            return size
+            return os.path.getsize(file_path)
         except OSError as e:
             print(f"Error: {e}")
             return None
 
-    def send_finished_recording_to_central_server(self, filename, filesize):
-        #TODO include file size etc.
-        self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/finished_recording", json.dumps({"deployed_sensor_id":self.deployed_sensor_id, "filename":filename,
-        "file_size": filesize}), qos=2)
+# CENTRAL SERVER COMMANDED STOP RECORDING
+
+    def stop_recording(self):
+        # Set a flag or a condition to stop the recording thread
+        self.stop_event.set()
+        self.client.publish(f"to_central_server/{self.deployed_sensor_id}/status/finished_recording", 
+                            "Recording Terminated by User",
+                            qos=1)
+
+#MAIN SENSOR TESTING
 
 def main():
     stop_event = threading.Event()
